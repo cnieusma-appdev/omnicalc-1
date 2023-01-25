f9c04db01f3f78cc09b2b7dcac841fbeef5095e1
cnieusma <cnieusma@uchicago.edu>
2023-01-25 00:06:16 +0000

    Fixing square root

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get("/square/results", { :controller => "application", :action => "calculate_square"})
 
   get("/square_root/new", { :controller => "application", :action => "blank_sqrt_form"})
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_sqrt"})
   
   get("/random/results",{ :controller => "application", :action => "calculate_random"})
 end
